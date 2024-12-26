11b3c83ed36b567c719185f52199fe19032775fd
Nupurbhoir <nupurbhoir21@gmail.com>
2024-12-26 10:18:07 +0530

    Add files via upload

---
+++ functn9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void swap(int x, int y) {
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    cout << "After Swapping in function x: " << x << " y: " << y << endl;
+}
+int main(){
+    int x=1, y=2;
+    cout << "Before Swapping: ";
+    cout << "x: " << x << ",y: " << y << endl;
+    swap(x,y);
+    cout << "After Swapping: ";
+    cout << "x: " << x << ",y: " << y << endl;
+    return 0;
+}
